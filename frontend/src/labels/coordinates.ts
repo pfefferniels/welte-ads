7e26fabe20e9d74233ac599858eba86642f580c4
Niels Pfeffer <47777840+pfefferniels@users.noreply.github.com>
2023-08-06 17:29:17 +0200

    add more encodings

--- frontend/src/labels/coordinates.ts
+++ frontend/src/labels/coordinates.ts
@@ -5,5 +5,6 @@ export const coordinates = {
     'https://www.geonames.org/2643743': { lat: 51.50853, long: -0.12574 },
     'https://www.geonames.org/2879139': { lat: 51.33962, long: 12.37129 },
     'https://www.geonames.org/2867714': { lat: 48.13743, long: 11.57549 },
-    'https://www.geonames.org/5368361': { lat: 34.05223, long: -118.24368}
+    'https://www.geonames.org/5368361': { lat: 34.05223, long: -118.24368 },
+    'https://www.geonames.org/4930956': { lat: 42.35843, long: -71.05977 }
 } as { [index: string]: Coordinate }
